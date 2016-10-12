7e255fe2e38c3b7c35ad03f8f7c10aa851efb916
kp1314 <kp1314@ic.ac.uk>
2016-10-12 23:48:21 +0100

    junit tests now built correctly with ant and should run fine on travis

---
+++ tests/QuadVTest.java
@@ -0,0 +1,11 @@
+import org.junit.Test;
+import junit.framework.*;
+import static org.junit.Assert.*;
+
+public class QuadVTest {
+    
+    @Test
+    public void Silly() {
+            assertEquals(1,1);
+    }
+}
