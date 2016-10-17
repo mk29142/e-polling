5c0dae4fcd6fcdba394740af8d5d500e830703f0
Rory Fayed <rf2114@point33.doc.ic.ac.uk>
2016-10-17 12:03:48 +0100

    MasterTree class added

--- src/main/java/MasterTree.java
+++ src/main/java/MasterTree.java
@@ -4,4 +4,24 @@ package main.java;
  * Created by rf2114 on 14/10/16.
  */
 public class MasterTree {
+
+    private Node root;
+
+    public MasterTree(Node root){
+        this.root = root;
+    }
+
+    public void updateScore(Argument newArgument, Node root){
+
+    }
+
+
+
+}
+
+
+class Node {
+
+    private int score;
+    private Argument root;
 }
