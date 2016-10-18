6dde82cbfa0a74de4980e6b99ed1f14e813b62ed
h22roscoe <harry.roscoe14@imperial.ac.uk>
2016-10-18 12:23:44 +0100

    Heroku stuff I think has been done already elsewhere

--- src/main/java/QuadV.java
+++ src/main/java/QuadV.java
@@ -3,9 +3,20 @@ import static spark.Spark.*;
 public class QuadV {
 	public static void main(String[] args) {
         // Configure Spark
-        port(4567);
+        port(getHerokuAssignedPort());
         staticFiles.location("/front-end/build");
 
         get("/hello", (request, response) -> "Hello Friend!");
 	}
+
+    private static int getHerokuAssignedPort() {
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        String port = processBuilder.environment().get("PORT");
+
+        if (port != null) {
+            return Integer.parseInt(port);
+        }
+
+        return 4567; // Return default port if heroku-port isn't set (i.e. on localhost)
+    }
 }
