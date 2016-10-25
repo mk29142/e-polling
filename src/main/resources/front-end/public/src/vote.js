394eb073b83ed766160a6676b1cc438ea680da24
sp5714 <sp5714@ic.ac.uk>
2016-10-25 13:44:13 +0100

    added button functionality to allow progression of questions

--- src/main/resources/front-end/public/src/vote.js
+++ src/main/resources/front-end/public/src/vote.js
@@ -26,6 +26,34 @@
   var reason = $('#opinion-area');
   var issue = questions[0];
   var counter = 1;
+  var currQ = questions[counter];
 
+  $('#finalQ').hide();
   title.html(issue.text);
+  question.html(currQ.text);
+
+  $('#nextQ').click(function(e) {
+    e.preventDefault();
+
+    currQ.support = slider.val();
+    currQ.reason = reason.val();
+
+    currQ = questions[++counter];
+    question.html(currQ.text);
+    slider.val(currQ.support);
+    reason.val(currQ.reason);
+
+    if (currQ.id === questions.length - 1) {
+      $('#nextQ').hide();
+      $('#finalQ').show();
+    }
+  });
+
+  $('#finalQ').click(function(e) {
+    e.preventDefault();
+
+    // Send back questions with ajax and redirect to results page
+
+    window.location.href = '/results';
+  });
 })();
