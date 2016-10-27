587f8367ec5eadfb6832176573b8c043ae0ba257
Kiran Patel <kp1314@ic.ac.uk>
2016-10-27 13:33:03 +0100

    trying to add database queries

--- src/main/java/QuadV.java
+++ src/main/java/QuadV.java
@@ -1,26 +1,54 @@
 //package main.java;
 
 import static spark.Spark.*;
+
 import java.sql.*;
 import java.math.*;
 import java.net.*;
 
 public class QuadV {
-	public static void main(String[] args) {
-        // Configure Spark
+    public static void main(String[] args) {
+        // Configure Spark and server routes.
         port(getPort());
+        Connection connection;
+
         try {
-           getConnection();
+            c = getConnection();
+
+            staticFiles.location("/front-end/public");
+
+            post("/create", (req, res) -> {
+                //Put Questions in the database tree structures for the first time
+                req.
+
+
+            });
+
+            get("/answer", (req, res) -> {
+                //Get all of the names of polls for listing
+                Statement stmt = connection.createStatement();
+
+                ResultSet rs = stmt.executeQuery("SELECT table_name FROM information_schema.tables WHERE table_schema='poll';");
+
+
+            });
+
+            get("/answer/:pollname", (req, res) -> {
+                //Get the questions one by one for the secific poll
+                //use PreparedStatement in here to stop string injection
+                String pollname = req.params(":pollname");
+		PreparedStatement findPolls = c.prepareStatement("SELECT  FROM polls"); 
+
+
+            });
+
+
         } catch (Exception e) {
-           
+
         }
+    }
 
-        staticFiles.location("/front-end/public");
-
-        get("/index", (req, res) -> "Hello World");
-	}
-
-    private static Connection getConnection() 
+    private static Connection getConnection()
             throws URISyntaxException, SQLException {
         String dbUrl = System.getenv("JDBC_DATABASE_URL");
         return DriverManager.getConnection(dbUrl);
