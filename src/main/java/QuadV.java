5aa1c8799b5d7098cdfee6af899c6fa8a0738fb8
har14 <har14@ic.ac.uk>
2016-10-28 13:58:41 +0100

    Added JSON stuff and can connect to heroku db. Now working on votingroom mustache

--- src/main/java/QuadV.java
+++ src/main/java/QuadV.java
@@ -2,6 +2,7 @@ import java.net.URISyntaxException;
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import static spark.Spark.*;
@@ -13,65 +14,104 @@ import spark.template.mustache.MustacheTemplateEngine;
 public class QuadV {
     public static void main(String[] args) {
         // Configure Spark and server routes.
-        port(getPort());
         Connection connection;
-        staticFiles.location("/front-end/public");
 
         try {
             connection = getConnection();
-
-            MustacheTemplateEngine templateEngine = new MustacheTemplateEngine();
-
-            get("/", (req, res) -> new ModelAndView(null, "index.mustache"), templateEngine);
-
-            get("/votingroom", (req, res) -> {
-                //Get all of the names of polls for listing
-                Statement stmt = connection.createStatement();
-                Map<String, ArrayList> map = new HashMap<>();
-                ResultSet rs = stmt.executeQuery("SELECT poll_name FROM public.polls");
-
-                ArrayList<String> output = new ArrayList<String>();
-                while (rs.next()) {
-                    output.add(rs.getString("poll_name"));
-                }
-                map.put("polls", output);
-                return new ModelAndView(map, "votingroom.mustache");
-            }, templateEngine);
-
-            get("/vote/:id", (request, response) -> {
-                Map<String, ArrayList> map = new HashMap<>();
-                //Get the questions one by one for the specific poll
-                //use PreparedStatement in here to stop string injection
-                PreparedStatement findPolls = connection.prepareStatement("SELECT poll.\"?\" FROM polls");
-                findPolls.setString(1, request.params(":id"));
-
-                ResultSet rs = findPolls.executeQuery();
-
-
-                return new ModelAndView(map, "vote.mustache");
-            }, templateEngine);
-
-            get("/create", (req, res) ->
-                            new ModelAndView(null, "create.mustache"),
-                    templateEngine);
-
-            get("/results", (req, res) ->
-                            new ModelAndView(null, "results.mustache"),
-                    templateEngine);
-        } catch (Exception e) {
-
+        } catch (URISyntaxException | SQLException e) {
+            System.out.println(e.getMessage());
+            return;
         }
+
+        port(getPort());
+
+        staticFiles.location("/front-end/public");
+
+        MustacheTemplateEngine templateEngine = new MustacheTemplateEngine();
+
+        get("/", (req, res) -> new ModelAndView(null, "index.mustache"), templateEngine);
+
+        get("/votingroom", (req, res) -> {
+            //Get all of the names of polls for listing
+            Statement stmt = connection.createStatement();
+            ResultSet rs = stmt.executeQuery("SELECT id, poll_name FROM public.polls");
+
+            List<Poll> polls = new ArrayList<>();
+
+            while (rs.next()) {
+                Poll p = new Poll(rs.getInt("id"), rs.getString("poll_name"));
+                polls.add(p);
+            }
+
+            return new ModelAndView(polls, "votingroom.mustache");
+        }, templateEngine);
+
+        get("/vote/:id", ((request, response) -> new ModelAndView(null, "vote.mustache")));
+
+        get("/boxes/:id", "application/json", (request, response) -> {
+            // Get the questions one by one for the specific poll
+            // use PreparedStatement in here to stop string injection
+            PreparedStatement findPolls = connection.prepareStatement("SELECT poll.\"?\" FROM polls");
+            findPolls.setString(1, request.params(":id"));
+            ResultSet rs = findPolls.executeQuery();
+            List<Box> boxes = new ArrayList<>();
+
+            while (rs.next()) {
+                int id = rs.getInt("id");
+                int parentId = rs.getInt("parentId");
+                String text = rs.getString("text");
+                String type = rs.getString("type");
+                boxes.add(new Box(id, parentId, text, type));
+            }
+
+            return boxes;
+        }, new JsonTransformer());
+
+        get("/create", (req, res) ->
+                new ModelAndView(null, "create.mustache"), templateEngine);
+
+        get("/results", (req, res) ->
+                new ModelAndView(null, "results.mustache"), templateEngine);
     }
 
     private static Connection getConnection()
             throws URISyntaxException, SQLException {
         String dbUrl = System.getenv("JDBC_DATABASE_URL");
-        return DriverManager.getConnection(dbUrl);
+        String user = "tcwufcwhobioex";
+        String pass = "AJ1O0Xgyo68mJT410unmRO-WIp";
+        try {
+            Class.forName("org.postgresql.Driver");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        return DriverManager.getConnection(dbUrl, user, pass);
     }
 
     private static int getPort() {
         ProcessBuilder processBuilder = new ProcessBuilder();
         String port = processBuilder.environment().get("PORT");
-        return port != null ? Integer.parseInt(port) : 4567;
+        return port != null ? Integer.parseInt(port) : 8080;
+    }
+
+    private static class Poll {
+        int id;
+        String name;
+
+        Poll(int id, String name) {
+            this.id = id;
+            this.name = name;
+        }
+    }
+
+    private static class Box {
+        int id, parentId;
+        String text, type;
+
+        Box(int id, int parentId, String text, String type) {
+            this.id = id;
+            this.parentId = parentId;
+            this.text = text;
+            this.type = type;
+        }
     }
 }
