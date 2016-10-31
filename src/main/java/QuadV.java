297eaa54a52cf615e76898b73ed6d10301cd0cfb
Kiran Patel <kp1314@ic.ac.uk>
2016-10-31 14:17:19 +0000

    poll names pulled correctl, questions are not pulled correctly from database howeve#r

--- src/main/java/QuadV.java
+++ src/main/java/QuadV.java
@@ -32,7 +32,9 @@ public class QuadV {
         get("/", (req, res) -> new ModelAndView(null, "index.mustache"), templateEngine);
 
         get("/votingroom", (req, res) -> {
-            //Get all of the names of polls for listing
+            // Get all of the names of polls for listing
+            Map<String, List<Poll>> map = new HashMap<>();
+
             Statement stmt = connection.createStatement();
             ResultSet rs = stmt.executeQuery("SELECT id, poll_name FROM public.polls");
 
@@ -43,25 +45,40 @@ public class QuadV {
                 polls.add(p);
             }
 
-            return new ModelAndView(polls, "votingroom.mustache");
+            map.put("polls", polls);
+
+            return new ModelAndView(map, "votingroom.mustache");
         }, templateEngine);
 
-        get("/vote/:id", ((request, response) -> new ModelAndView(null, "vote.mustache")));
+        get("/vote/:id", ((request, response) -> new ModelAndView(null, "vote.mustache")), templateEngine);
 
         get("/boxes/:id", "application/json", (request, response) -> {
             // Get the questions one by one for the specific poll
             // use PreparedStatement in here to stop string injection
-            PreparedStatement findPolls = connection.prepareStatement("SELECT poll.\"?\" FROM polls");
-            findPolls.setString(1, request.params(":id"));
-            ResultSet rs = findPolls.executeQuery();
+            ResultSet rs = null;
+            try {
+                PreparedStatement findPoll = connection.prepareStatement("SELECT poll_name FROM polls WHERE id = ?;");
+                findPoll.setInt(1, Integer.parseInt(request.params(":id")));
+                rs = findPoll.executeQuery();
+
+                rs.next();
+                String name =  rs.getString("poll_name");
+                PreparedStatement findStatements = connection.prepareStatement("SELECT poll_id, parent_id, statement, type FROM poll.?;");
+                findStatements.setString(1, name);
+                System.out.println(findStatements.toString());
+                rs = connection.createStatement().executeQuery(findStatements.toString());
+            } catch (SQLException e) {
+                System.out.println(e.getMessage());
+                return e.getMessage();
+            }
             List<Box> boxes = new ArrayList<>();
 
             while (rs.next()) {
-                int id = rs.getInt("id");
-                int parentId = rs.getInt("parentId");
-                String text = rs.getString("text");
+                int id = rs.getInt("poll_id");
+                int parentId = rs.getInt("parent_id");
+                String statement = rs.getString("statement");
                 String type = rs.getString("type");
-                boxes.add(new Box(id, parentId, text, type));
+                boxes.add(new Box(id, parentId, statement, type));
             }
 
             return boxes;
@@ -95,15 +112,20 @@ public class QuadV {
             this.id = id;
             this.name = name;
         }
+
+        @Override
+        public String toString() {
+            return "Poll = " + name + " with id " + id;
+        }
     }
 
     private static class Box {
-        int id, parentId;
+        int id, parent;
         String text, type;
 
         Box(int id, int parentId, String text, String type) {
             this.id = id;
-            this.parentId = parentId;
+            this.parent = parentId;
             this.text = text;
             this.type = type;
         }
