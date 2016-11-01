681b844b8de756068e733f0c90118c65e9865f10
sp5714 <sp5714@ic.ac.uk>
2016-11-01 14:00:03 +0000

    changed string type

--- src/main/resources/front-end/public/src/create.js
+++ src/main/resources/front-end/public/src/create.js
@@ -1,5 +1,30 @@
 (function() {
-  new Box({
-    id: id++
+
+  var boxOptions = {
+    id: id++,
+    parentId: null,
+    type: 'Issue'
+  };
+
+  new Box(boxOptions);
+
+  list.push(boxOptions);
+
+  $('#submit-poll').click(function() {
+    for (var i = 0; i < list.length; i++) {
+      var elem = list[i];
+      elem.value = $('#box' + elem.id).find('input').val();
+    }
+    $.ajax({
+      type: 'POST',
+      url: '/create',
+      data: {
+        name: list[0].value,
+        list: list
+      },
+      success: function() {
+        console.log('Succeeded');
+      }
+    });
   });
 })();
