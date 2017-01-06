cae02364e045f289bd161f17c5a450c47bb3ff4a
kp1314 <kp1314@ic.ac.uk>
2017-01-06 22:54:53 +0000

    If a sentence inputted is much longer than a sentence you are comparing then make them the same length by using the longest words of the longer sentence

--- src/main/java/NLPUtils.java
+++ src/main/java/NLPUtils.java
@@ -1,14 +1,22 @@
+import edu.cmu.lti.jawjaw.pobj.POS;
+import edu.cmu.lti.jawjaw.util.WordNetUtil;
 import edu.cmu.lti.lexical_db.ILexicalDatabase;
 import edu.cmu.lti.lexical_db.NictWordNet;
+import edu.cmu.lti.lexical_db.data.Concept;
+import edu.cmu.lti.ws4j.Relatedness;
 import edu.cmu.lti.ws4j.RelatednessCalculator;
 import edu.cmu.lti.ws4j.impl.HirstStOnge;
 import edu.cmu.lti.ws4j.impl.Lin;
 import edu.cmu.lti.ws4j.impl.Path;
 import edu.cmu.lti.ws4j.impl.WuPalmer;
+import edu.cmu.lti.ws4j.util.PorterStemmer;
 import edu.cmu.lti.ws4j.util.WS4JConfiguration;
+import org.apache.commons.lang3.StringUtils;
 
 import java.util.Arrays;
+import java.util.List;
 import java.util.StringTokenizer;
+import java.util.stream.Collector;
 
 class NLPUtils {
     private static ILexicalDatabase db = new NictWordNet();
@@ -17,17 +25,28 @@ class NLPUtils {
         Pair<double[]> stringVectors = stringsToVectors(oldArg, newArg);
 
         // Use cosineSimilarity to analyse these vectors
-        return cosineSimilarity(stringVectors.getOne(), stringVectors.getTwo());
+        double[] one = stringVectors.getOne();
+        double[] two = stringVectors.getTwo();
+
+        if (allZero(one) || allZero(two)) return 0;
+
+        return cosineSimilarity(one, two);
+    }
+
+    private static boolean allZero(double[] arr) {
+        return Arrays.stream(arr).allMatch(value -> value == 0);
     }
 
     private static String removeStopWordsAndStem(String string) {
         StringBuilder result = new StringBuilder();
         StringTokenizer st = new StringTokenizer(string);
 
+        PorterStemmer ps = new PorterStemmer();
+
         while (st.hasMoreTokens()) {
             String next = st.nextToken();
             if (!isStopWord(next)) {
-                // Here we could stem and lemmatize the words
+
                 result.append(next);
                 result.append(' ');
             }
@@ -56,27 +75,41 @@ class NLPUtils {
     }
 
     private static Pair<double[]> stringsToVectors(String s1, String s2) {
-        String[] concatString = removeRepAndStopWords(s1 + ' ' + s2);
         String[] word1 = removeRepAndStopWords(s1);
         String[] word2 = removeRepAndStopWords(s2);
 
-        return new Pair<>(
-            makeVector(concatString, word1),
-            makeVector(concatString, word2)
-        );
+        if (word1.length > word2.length) {
+            word1 = simplifyLargeSentence(word1, word2);
+        } else {
+            word2 = simplifyLargeSentence(word2, word1);
+        }
+
+        return new Pair<>(makeVector(word2, word1), makeVector(word1, word2));
     }
 
-    private static double[] makeVector(String[] concat, String[] word) {
-        double[] ret = new double[concat.length];
+    private static String[] simplifyLargeSentence(String[] larger, String[] smaller) {
+        return Arrays.stream(larger)
+            .sorted((a, b) -> a.length() > b.length() ? 1 : -1)
+            .limit(smaller.length).toArray(String[]::new);
+    }
 
-        for (int i = 0; i < concat.length; i++) {
+    private static double[] makeVector(String[] sentence1, String[] sentence2) {
+        double[] ret = new double[sentence1.length];
+
+        for (int i = 0; i < sentence1.length; i++) {
             double sum = 0;
+            int nonZeros = 0;
 
-            for (String w : word) {
-                sum += relatedness(concat[i], w);
+            for (String w : sentence2) {
+                double r = relatedness(sentence1[i], w);
+                sum += r;
+
+                if (r > 0) {
+                    nonZeros++;
+                }
             }
 
-            ret[i] = sum / word.length;
+            ret[i] = nonZeros > 0 ? sum / nonZeros : 0;
         }
 
         return ret;
@@ -116,37 +149,54 @@ class NLPUtils {
     static double relatedness(String word1, String word2) {
         WS4JConfiguration.getInstance().setMFS(true);
         RelatednessCalculator[] rcs = { new WuPalmer(db), new Path(db), new Lin(db) };
-        double[] weights = { 0.6, 0.1, 0.2 };
-        double sum = 0;
-
-        RelatednessCalculator hso = new HirstStOnge(db);
-        double hsoWeight = 0.0625 * 0.1;
-        double hsoVal = hso.calcRelatednessOfWords(word1, word2);
-
-        if (hsoVal >= 0 && hsoVal <= 1) {
-            sum += hsoVal * hsoWeight;
-        } else {
-            sum += 0.1;
-        }
+        double[] weights = { 0.4, 0.4, 0.2 };
+        double[] scores = { 0, 0, 0 };
 
         for (int i = 0; i < rcs.length; i++) {
-            double value = rcs[i].calcRelatednessOfWords(word1, word2);
-            if (value >= 0 && value <= 1) {
-                sum += value * weights[i];
-            } else if (value > 1) {
-                sum += weights[i];
+            List<POS[]> posPairs = rcs[i].getPOSPairs();
+
+            for (POS[] posPair : posPairs) {
+
+                List<Concept> synset1 =
+                    (List<Concept>) db.getAllConcepts(word1, posPair[0].toString());
+                List<Concept> synset2 =
+                    (List<Concept>) db.getAllConcepts(word2, posPair[1].toString());
+
+                for (Concept c1 : synset1) {
+                    for (Concept c2 : synset2) {
+                        double value = rcs[i].calcRelatednessOfSynset(c1, c2).getScore();
+
+                        if (value > scores[i] && value > 0) {
+                            scores[i] = value;
+                        }
+                    }
+                }
             }
         }
 
-        return sum;
+        scores = Arrays.stream(scores)
+            .map(value -> value > 1 ? 1 : value)
+            .toArray();
+        return dotProduct(scores, weights);
     }
 
     private static boolean isStopWord(String string) {
-        String[] stopArray = new String[]{"a", "an", "and", "are", "as", "at", "be", "but", "by",
-                "for", "if", "in", "into", "is", "it",
+        String[] stopArray = {
+            "a", "all", "am", "any", "aren't", "an", "about",
+                "above", "after", "again", "against",
+                "and", "are", "as", "at", "be", "because", "been", "before",
+                "below", "between", "both", "but", "by", "can't", "cannot",
+                "could", "couldn't", "did", "didn't", "do", "does", "do",
+                "doesn't", "doing", "don't", "down", "during", "each", "few",
+                "from", "further", "for", "had", "hadn't", "has", "hasn't", "have",
+                "haven't", "having", "he", "he'd", "he'll", "he's", "her", "here",
+                "here's", "hers", "herself", "him", "himself", "his", "how",
+                "how's", "i", "i'd", "i'll", "i'm", "i've", "if", "in", "into", "is", "isn't",
+                "if", "in", "into", "is", "it",
                 "no", "not", "of", "on", "or", "such",
                 "that", "the", "their", "then", "there", "these",
-                "they", "this", "to", "was", "will", "with"};
+                "they", "this", "to", "was", "will", "with"
+        };
 
         return Arrays.asList(stopArray).contains(string);
     }
--- src/test/java/QuadVTest.java
+++ src/test/java/QuadVTest.java
@@ -7,7 +7,7 @@ public class QuadVTest {
 
     @Test
     public void checkStringSaysTwoSimilarHaveHighSimilarity() {
-        String otherCompareStr = "Kante has football skills that are brilliant";
+        String otherCompareStr = "Kante is a very good player of football";
         double val =
                 NLPUtils.checkStrings(compareStr, otherCompareStr);
         System.out.println(val);
@@ -16,15 +16,30 @@ public class QuadVTest {
 
     @Test
     public void checkStringSaysNotSimilarLowSimilarity() {
-        String notSimilarStr = "Where is the nearest post office, Madam?";
+        String notSimilarStr = "Every day of my life I eat a lot of lemons and drink water with beans and bear snacks";
         double val = NLPUtils.checkStrings(notSimilarStr, compareStr);
         System.out.println(val);
         assertTrue(val <= 0.5);
     }
 
     @Test
+    public void checkSimilarSentence() {
+        String comparator = "Kante is great at sports and has nice technique";
+        double val = NLPUtils.checkStrings(compareStr, comparator);
+        System.out.println(val);
+    }
+
+    @Test
+    public void checkSentencesAreTheSame() {
+        String comparator = "Kante is a great football player";
+        double val = NLPUtils.checkStrings(compareStr, comparator);
+        System.out.println(val);
+    }
+
+    @Test
     public void relatedGivesValuesBetweenZeroAndOneWithSameWord() {
         double sameWord = NLPUtils.relatedness("Word", "worD");
+        System.out.println(sameWord);
         assertTrue(sameWord <= 1.0);
         assertTrue(sameWord >= 0.0);
     }
@@ -32,13 +47,14 @@ public class QuadVTest {
     @Test
     public void relatedGivesValuesBetweenZeroAndOneForDiffWords() {
         double veryDiffWords = NLPUtils.relatedness("HEllo", "Giraffe");
+        System.out.println(veryDiffWords);
         assertTrue(veryDiffWords <= 1);
         assertTrue(veryDiffWords >= 0);
     }
 
     @Test
-    public void checkWordsRelatedness() {
-        double r = NLPUtils.relatedness("goodbye", "hello");
+    public void checkDropFallRelatedness() {
+        double r = NLPUtils.relatedness("drop", "fall");
         System.out.println(r);
         assertTrue(r > 0);
     }
