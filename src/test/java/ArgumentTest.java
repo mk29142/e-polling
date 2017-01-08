5af0f224b5871e820764ebe687dccc91f11109b6
Rory <rory.fayed14@imperial.ac.uk>
2017-01-08 18:43:47 +0000

    added ArgumentTest class that tests methods in the Argument class

---
+++ src/test/java/ArgumentTest.java
@@ -0,0 +1,44 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.instanceOf;
+import static org.junit.Assert.*;
+
+
+public class ArgumentTest {
+    private Argument arg;
+
+    @Before
+    public void initialise() {
+        arg = new Argument(true, "Britain should leave the EU",false);
+        Argument child1 = new Argument(true, "EU migrants are good for the economy", false);
+        Argument child2 = new Argument(true, "EU migrants pay more tax than they take out", false);
+        arg.addChild(child1);
+        arg.addChild(child2);
+    }
+
+    @Test
+    public void canAddChildren() {
+        assertEquals(2, arg.getChildren().size());
+    }
+
+    @Test
+    public void checkArgumentTreeIsInconsistent() {
+        assertNotNull(arg.getInconsistencies());
+    }
+
+    @Test
+    public void toBoxReturnsBoxObject(){
+        assertThat(arg.toBox(), instanceOf(Box.class));
+    }
+
+
+    @Test
+    public void updateArgumentTreeScores() {
+
+
+
+    }
+
+
+}
